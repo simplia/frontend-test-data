8ece1faa3ee2636c094c7d6ef4c2098d859f5885
Petr Soukup <soukup@simplia.cz>
2022-08-09 08:30:12 +0200

    return null if no url exists

--- src/Url.ts
+++ src/Url.ts
@@ -7,7 +7,7 @@ export default class Url {
         return `${process.env.BASE_URL}`.replace('https://', '')
     }
 
-    public static async index(config?: Config): Promise<string> {
+    public static async index(config?: Config): Promise<string|null> {
         if (!config) {
             return new Promise((resolve) => {
                 resolve(`${process.env.BASE_URL}/`)
@@ -16,23 +16,23 @@ export default class Url {
         return Api.single('index', config);
     }
 
-    public static async productWithoutVariants(config?: Config): Promise<string> {
+    public static async productWithoutVariants(config?: Config): Promise<string|null> {
         return Api.single('product-without-variant', config);
     }
 
-    public static async productWithSingleVariant(config?: Config): Promise<string> {
+    public static async productWithSingleVariant(config?: Config): Promise<string|null> {
         return Api.single('product-single-active-variant', config);
     }
 
-    public static async productWithSingleSelectedVariant(config?: Config): Promise<string> {
+    public static async productWithSingleSelectedVariant(config?: Config): Promise<string|null> {
         return Api.single('product-single-active-selected-variant', config);
     }
 
-    public static async productWithVariants(config?: Config): Promise<string> {
+    public static async productWithVariants(config?: Config): Promise<string|null> {
         return Api.single('product-with-variants', config);
     }
 
-    public static async productWithVariantsSelected(config?: Config): Promise<string> {
+    public static async productWithVariantsSelected(config?: Config): Promise<string|null> {
         return Api.single('product-with-variants-selected', config);
     }
 }
